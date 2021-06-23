d85cf59a3c532493182a151bea11b6a7eb8f322f
amamov <ysangsuk78@gmail.com>
2021-06-24 01:45:12 +0900

    [update] repository pattern

--- pre-dev/backend/src/app.ts
+++ pre-dev/backend/src/app.ts
@@ -1,11 +1,14 @@
 import * as express from 'express';
+import { CatType, Cat } from './app.repository';
 
 const app: express.Express = express();
+
+app.use(express.json()); // body가 json 타입을 읽을 수 있도록 설정을 해주어야 한다.
 
 app.get('/', (req: express.Request, res: express.Response) => {
   console.dir(req);
   console.log(req.header);
-  res.send({ hello: 'world' });
+  res.send({ hello: 'world', data: Cat });
 });
 
 app.listen(8000, () => {
