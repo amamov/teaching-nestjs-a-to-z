d9b59b38fbceeeaaa7715fa01a3863cb9b026028
amamov <ysangsuk78@gmail.com>
2021-06-28 00:25:05 +0900

    [create] 04

---
+++ 04 NestJS 개요 및 객체지향 디자인 패턴/07/src/app.module.ts
@@ -0,0 +1,16 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ 05 NestJS 실전 프로젝트/01/src/cats/cats.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Delete,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  // cats/
+  @Get()
+  getAllCat() {
+    console.log('hello controller');
+    return { cats: 'get all cat api' };
+  }
+
+  // cats/:id
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
+    return 'get one cat api';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat api';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat api';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update partial cat api';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete service';
+  }
+}
