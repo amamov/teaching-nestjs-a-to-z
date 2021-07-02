001c1882d3f5d871306239cfaca008d8421ecf5a
amamov <ysangsuk78@gmail.com>
2021-07-02 23:40:58 +0900

    [update] 6 start

---
+++ 06 프로젝트 배포와 서버 운영/src/auth/jwt/jwt.payload.ts
@@ -0,0 +1,4 @@
+export type Payload = {
+  email: string;
+  sub: string;
+};
---
+++ 06 프로젝트 배포와 서버 운영/src/cats/cats.schema.ts
@@ -0,0 +1,73 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Document, SchemaOptions } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Cat extends Document {
+  @ApiProperty({
+    example: 'amamov@kakao.com',
+    description: 'email',
+    required: true,
+  })
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    example: 'amamov',
+    description: 'name',
+    required: true,
+  })
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    example: '23810',
+    description: 'password',
+    required: true,
+  })
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Prop({
+    default:
+      'https://github.com/amamov/NestJS-solid-restapi-boilerplate/raw/main/docs/images/1.jpeg',
+  })
+  @IsString()
+  imgUrl: string;
+
+  readonly readOnlyData: {
+    id: string;
+    email: string;
+    name: string;
+    imgUrl: string;
+  };
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
+
+CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+  return {
+    id: this.id,
+    email: this.email,
+    name: this.name,
+    imgUrl: this.imgUrl,
+  };
+});
