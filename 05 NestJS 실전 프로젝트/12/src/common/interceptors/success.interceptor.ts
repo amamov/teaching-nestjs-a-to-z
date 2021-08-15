02f15c016aee755bc27545bbb24f89035843933b
amamov <amamov@kakao.com>
2021-08-15 17:01:12 +0900

    [delete] .env and create bonus

---
+++ 05 NestJS 실전 프로젝트/12/src/common/interceptors/success.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class SuccessInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        data,
+      })),
+    );
+  }
+}
