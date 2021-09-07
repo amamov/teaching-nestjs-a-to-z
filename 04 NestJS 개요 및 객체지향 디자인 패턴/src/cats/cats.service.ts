fb4434b4900eb44db1d0d68144b8c82bffc7cbef
amamov <amamov@kakao.com>
2021-09-07 12:13:13 +0900

    [create] frontend code

--- 04 NestJS 개요 및 객체지향 디자인 패턴/src/cats/cats.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class CatsService {
-  hiCatServiceProduct() {
-    return 'hello cat!';
-  }
-}
--- 04 NestJS 개요 및 객체지향 디자인 패턴/src/common/middlewares/logger.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LoggerMiddleware } from './logger.middleware';
-
-describe('LoggerMiddleware', () => {
-  it('should be defined', () => {
-    expect(new LoggerMiddleware()).toBeDefined();
-  });
-});
